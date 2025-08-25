70c0762c903edc13b89cd17e9aadaad0475dc97d
KiranBalasoPatil3052006 <kiranbalasopatil33@gmail.com>
2025-08-25 20:53:47 +0530

    Initial commit full smart-cart-app

---
+++ www/js/config.js
@@ -0,0 +1,4 @@
+let backendBaseURL = "https://00abfc9749c2.ngrok-free.app";
+
+alert("Base URL: " + backendBaseURL); // REMOVE only after confirming!
+console.log("✅ Backend URL in use:", backendBaseURL);
